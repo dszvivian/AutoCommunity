b54bcb3c6760ed627bb923dc9329767bba3dfd87
dszvivian <dszvivian@gmail.com>
2023-05-18 21:37:54 +0530

    fragments added but not displaying

--- app/src/main/java/com/example/autocommunity/MainActivity.java
+++ app/src/main/java/com/example/autocommunity/MainActivity.java
@@ -1,37 +1,60 @@
 package com.example.autocommunity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
-
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import android.widget.TextView;
-
-import com.example.autocommunity.api.ApiService;
-import com.example.autocommunity.api.RetrofitClient;
 import com.example.autocommunity.model.Results;
+import com.example.autocommunity.pages.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Retrofit retrofit = null;
+    BottomNavigationView bnv;
 
-    TextView tvData;
+    HomeFragment homeFragment;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvData = findViewById(R.id.tvData);
+
+        bnv = findViewById(R.id.bnvMain);
+        homeFragment = new HomeFragment();
 
         connect();
 
+        setBnv();
+
+    }
+
+    private void setBnv() {
+        bnv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.miHome:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragmentContainerView, homeFragment)
+                                .commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     private void connect() {
@@ -40,7 +63,8 @@ public class MainActivity extends AppCompatActivity {
         vm.getData().observe(this, new Observer<Results>() {
             @Override
             public void onChanged(Results results) {
-                tvData.setText(results.getName());
+                // todo: set the textView here
+                /*tvData.setText(results.getName());*/
             }
         });
 
---
+++ app/src/main/java/com/example/autocommunity/adapters/HomePageAdapter.java
@@ -0,0 +1,71 @@
+package com.example.autocommunity.adapters;
+
+import static android.os.Build.VERSION_CODES.R;
+
+import static com.example.autocommunity.R.id;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.autocommunity.R;
+import com.example.autocommunity.model.HomePageItemsModel;
+
+import java.util.List;
+
+public class HomePageAdapter extends RecyclerView.Adapter<HomePageAdapter.ViewHolder> {
+
+    R r ;
+    private List<HomePageItemsModel> list;
+
+    public HomePageAdapter(List<HomePageItemsModel> list){
+        this.list = list;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(com.example.autocommunity.R.layout.item_homepage,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        HomePageItemsModel item = list.get(position);
+        holder.post.setImageResource(item.getPostImage());
+        holder.profileIcon.setImageResource(item.getProfileImage());
+        holder.username.setText(item.getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView post,profileIcon;
+        TextView username;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            post = itemView.findViewById(id.ivHomePost);
+            profileIcon = itemView.findViewById(id.ivHomeProfile);
+            username = itemView.findViewById(id.tvHomeUsername);
+
+        }
+    }
+
+
+
+
+}
+
+
